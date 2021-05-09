d6d92008809dfb3fe0030db1fa9e50e452df60a0
Liam <mentalmuso@gmail.com>
2021-05-08 23:11:58 -0500

    SELinux Always permissive or enforcing
    (cherry picked from commit e7cac9bd6896422cf0a3b2b9ac42a9222944015c)
    (cherry picked from commit 06fe8a0746cbb8cb8e40cb471f1e55a30351e77a)
    (cherry picked from commit 85918ea485f14c4ab279f9d91e19a9e6bed2bc29)
    (cherry picked from commit 0018fef271663782ac6566950011f4ddaa8d3563)

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -1667,7 +1667,7 @@ out:
 	kfree(t);
 	kfree(n);
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	selinux_enforcing = 1;
 #endif
 	if (!selinux_enforcing) // SEC_SELINUX_PORTING_COMMON Change to use RKP 
@@ -1970,7 +1970,7 @@ static inline int convert_context_handle_invalid_context(
 	u32 len;
 
 // [ SEC_SELINUX_PORTING_COMMON 
-	#ifdef CONFIG_ALWAYS_ENFORCE
+	#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	    selinux_enforcing = 1;
 	#endif
 	if (!selinux_enforcing) // SEC_SELINUX_PORTING_COMMON Change to use RKP
